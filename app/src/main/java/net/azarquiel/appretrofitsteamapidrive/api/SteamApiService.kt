b33642809f3603f03c309630e0032a0907e8ed8b
Oscar Muñoz Moreno <themelocoton@gmail.com>
2018-02-08 21:37:11 +0100

    Añadidas interfaces y DataClass

--- app/src/main/java/net/azarquiel/appretrofitsteamapidrive/api/SteamApiService.kt
+++ app/src/main/java/net/azarquiel/appretrofitsteamapidrive/api/SteamApiService.kt
@@ -1,10 +1,29 @@
 package net.azarquiel.appretrofitsteamapidrive.api
 
+import net.azarquiel.appretrofitsteamapidrive.model.Games
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import rx.Observable
+
 /**
  * Created by Oscar on 06/02/2018.
  */
 interface SteamApiService {
 
+    @GET("ISteamApps/GetAppList/v0002/")
+    fun getData(): Observable<Games>
 
+    companion object {
+        fun create(): SteamApiService {
+            val retrofit = Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                    .baseUrl("http://api.steampowered.com/")
+                    .build()
+            return retrofit.create(SteamApiService::class.java)
+        }
+    }
 
 }
