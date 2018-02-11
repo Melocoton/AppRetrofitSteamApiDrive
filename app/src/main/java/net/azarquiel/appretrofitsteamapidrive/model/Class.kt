3e597cf6bdb56c5a03e1a291f45269a9f536624c
Oscar Muñoz Moreno <themelocoton@gmail.com>
2018-02-11 19:39:16 +0100

    Archivos arreglados

--- app/src/main/java/net/azarquiel/appretrofitsteamapidrive/model/Class.kt
+++ app/src/main/java/net/azarquiel/appretrofitsteamapidrive/model/Class.kt
@@ -1,20 +1,20 @@
 package net.azarquiel.appretrofitsteamapidrive.model
 
-/**
- * Created by Oscar on 06/02/2018.
- */
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Created by Oscar on 11/02/2018.
+ */
 // Class para la SteamApiService Get todos los games
 data class Games(val applist:Apps)
 data class Apps(val apps:List<Game>)
-data class Game(val appid:String, val name:String)
+data class Game(val appid:String,
+                val name:String)
 
 // Class para la SteamStoreService Get un solo game
 data class GameStore(val data:Data)
-data class Data(val aap_id:Int,
-                val name:String,
-                val about_the_game:String,
+data class Data(val name:String,
+                val short_description:String,
                 val header_image:String,
                 val website:String)
 
@@ -23,7 +23,6 @@ data class DriveResponse (var table:Table)
 data class Table (var rows : List<Rows>)
 data class Rows (@SerializedName("c") var column : List<Column>)
 data class Column (@SerializedName("v") var value : String, @SerializedName("f") var format : String)
-
 // class para volcar Games de Drive que están en Rows y Columns
 // en Objetos mas bonitos. O uno de SteamStore que está en Data
 data class GameDrive(var id:String="", var name:String = "",var descripcion:String="", var image:String="", var link:String="")
